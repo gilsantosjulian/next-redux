741a41224582fafc111760009e3da4abf7827e86
Julian Gil <jgil@hugeinc.com>
2021-08-06 19:14:53 -0500

    feat: add todo component

---
+++ pages/todo.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import { connect } from 'react-redux'
+import { setInfo } from '../redux/actions/main'
+
+const Todo = () => {
+  return(
+      <div>
+        <Head>
+          <title>The Todo</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+      <main>
+        <div>  
+          <h1>This is a todo</h1>
+        </div>
+      </main>
+      </div>
+  )
+}
+
+export default Todo
