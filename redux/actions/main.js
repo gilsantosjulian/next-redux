be0438ba1d8011eaa73ccd6e3c823844bf0f7cb2
Julian Andres Gil Santos <gilsantosjulian@gmail.com>
2021-08-13 22:21:29 -0500

    feat: add userSignIn redux action

--- redux/actions/main.js
+++ redux/actions/main.js
@@ -1,8 +1,41 @@
 import * as t from '../type'
 
+const { API_ADDRESS } = process.env
+
 export const setInfo = (name) => {
   return ({
     type: t.SET_TYPE,
     payload: name
   })
+}
+
+export const userSignIn = ({ email, password }) => async dispatch => {
+  try {
+    dispatch({
+      type: t.LOADING,
+      payload: true
+    })
+    const url = `${API_ADDRESS}/api/auth/login`
+    const apiResponse = await axios.post(url, { email, password })
+    const { success, user } = apiResponse?.data
+
+    if(success) {
+      localStorage.setItem('user_info', JSON.stringify(user))
+      dispatch({
+        type: t.REGISTER,
+        payload: user
+      })
+    }
+  } catch (error) {
+    console.log(error.response)
+    dispatch({
+      type: t.LOADING,
+      payload: false
+    })
+    dispatch({
+      type: t.ERROR,
+      payload: error.response.data.error
+    })
+  }
+
 }
