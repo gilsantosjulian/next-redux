281e3584f0aa2d90d3f8364f9a6a2d40be31cc51
Julian Gil <jgil@hugeinc.com>
2021-08-20 20:20:55 -0500

    feat: add update - my - delete auth routes

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
+const validate = require('../middlewares/validate')
 const TodoSchema = require('../model/schemas/todo.js')
-
 
 const routes = () => {
 
@@ -10,7 +10,6 @@ const routes = () => {
 
   router.post('/new', (req, res) => {
     const todo = req.body
-
     const newTodo = new TodoSchema(todo)
 
     newTodo.save()
@@ -25,19 +24,64 @@ const routes = () => {
           }
         })
       })
-      .catch(error => res.status(500).json({ success: false, error: error }))
+      .catch(
+        error => res.status(500).json({ success: false, error: error })
+      )
   })
 
   router.post('/my', (req, res) => {
+    const info = req.body
 
+    TodoSchema.find({ email: info.email })
+      .then(todo => {
+        res.status(200).json({
+          success: true,
+          todo: todo,
+        })
+      })
+      .catch(
+        error => res.status(500).json({ success: false, error: error })
+      )
   })
 
-  router.post('/update', (req, res) => {
+  router.post('/update', async (req, res) => {
+    const info = req.body
+    try {
+      const todo = await TodoSchema.updateOne({_id: info.id}, {
+        $set: {
+          title: info.title,
+          done: info.done,
+        }
+      })
 
+      if(todo) {
+        const todo = await TodoSchema.find({ email: info.email })
+
+        res.status(200).json({
+          success: true,
+          todo: todo,
+        })
+      }
+    } catch (error) {
+      res.status(500).json({ success: false, error: error })
+    }
   })
 
-  router.post('/delete', (req, res) => {
+  router.post('/delete', validate, async (req, res) => {
+    const info = req.body
+    try {
+      const todo = await TodoSchema.deleteOne({ _id: info.id })
 
+      if(todo) {
+        const todos = await TodoSchema.find({ email: info.email })
+        res.status(200).json({
+          success: true,
+          todos: todos,
+        })
+      }
+    } catch (error) {
+      res.status(500).json({ success: false, error: error })
+    }
   })
 
   return router;
